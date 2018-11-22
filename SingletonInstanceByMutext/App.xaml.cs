d5fa99905fd79d7612b915c97cc4383f67272703
biancheng1000 <drogensection@hotmail.com>
2018-11-22 15:46:38 +0800

    单件实例

---
+++ SingletonInstanceByMutext/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SingletonInstanceByMutext
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private static Mutex _mutex = null;
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            const string appName = "MyAppName";
+            bool createdNew;
+            _mutex = new Mutex(true, appName, out createdNew);
+
+            if (!createdNew)
+            {
+                MessageBox.Show("程序已经运行！"); 
+                Application.Current.Shutdown();
+            }
+
+            base.OnStartup(e);
+        }
+    }
+}
